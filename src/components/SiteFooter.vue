7c00aae2e9d36f860dd9aaadf0e9fdcd42693211
Garrett Vorbeck <gvorbeck@gmail.com>
2024-04-12 18:41:31 -0400

    favicons

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,6 +1,18 @@
 <template>
-  <p class="text-sm text-justify">
-    I like to make lists and I like to listen to music. Not always at the same time. Here are my
-    favorite albums of the year. For a lot of years!
-  </p>
+  <div class="[&_p]:text-sm [&_p]:text-justify [&_*+*]:mt-2">
+    <p>
+      I like to make lists and I like to listen to music. Not always at the same time. Here are my
+      favorite albums of the year. For a lot of years!
+    </p>
+    <hr />
+    <p>
+      This website serves as a portfolio project that demonstrates a comprehensive understanding of
+      Vue.js. It features an extensive use of components, props, data handling, computed properties,
+      methods, watchers, event emitters, API integrations, and lifecycle hooks. Additionally, the
+      project leverages TypeScript and Tailwind CSS for robust and scalable development. Integration
+      with the Last.fm API showcases real-time data fetching and manipulation. Explore the source
+      code on <a>GitHub</a> to learn more about the implementation and to view the codebase.
+    </p>
+    <p>Contact: <a href="mailto:me@iamgarrett.com">J. Garrett Vorbeck</a></p>
+  </div>
 </template>
