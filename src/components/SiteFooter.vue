e57e665e746d171b6f674d03d128df7b709eef4f
Garrett Vorbeck <gvorbeck@gmail.com>
2024-04-13 23:38:43 -0400

    Add deploy script to package.json

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -11,8 +11,8 @@
       methods, watchers, event emitters, API integrations, and lifecycle hooks. Additionally, the
       project leverages TypeScript and Tailwind CSS for robust and scalable development. Integration
       with the Last.fm API showcases real-time data fetching and manipulation. Explore the source
-      code on <a target="_blank">GitHub</a> to learn more about the implementation and to view the
-      codebase.
+      code on <a href="https://github.com/gvorbeck/opinions-on-music" target="_blank">GitHub</a> to
+      learn more about the implementation and to view the codebase.
     </p>
     <p>Contact: <a href="mailto:me@iamgarrett.com">J. Garrett Vorbeck</a></p>
   </div>
